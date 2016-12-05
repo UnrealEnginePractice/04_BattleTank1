fafe30c33e2c8a0ab43235c5a54464bb3e229321
BELLA\spidy_000 <spidyblack@sbcglobal.net>
2016-12-05 15:46:44 -0800

    Created New File For BATTLETANK GAME

---
+++ BattleTankGame/Source/BattleTankGame/BattleTankGameGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "BattleTankGameGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANKGAME_API ABattleTankGameGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
