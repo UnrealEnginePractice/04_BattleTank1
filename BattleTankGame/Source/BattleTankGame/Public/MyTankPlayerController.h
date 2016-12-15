29fb793c6d426a18bbd4787c6866330dd8caf2fe
BELLA\spidy_000 <spidyblack@sbcglobal.net>
2016-12-14 17:56:33 -0800

    Added main menu Created Header file

---
+++ BattleTankGame/Source/BattleTankGame/Public/MyTankPlayerController.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/PlayerController.h"
+#include "MyTankPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANKGAME_API AMyTankPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
